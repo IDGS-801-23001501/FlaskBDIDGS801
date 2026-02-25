fdcce7bf40f744e04163218a3a91ba4e5071665a
IDGS-801-23001501 <idgs.23001501@gmail.com>
2026-02-25 11:58:18 -0600

    ABC Completo

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_migrate import Migrate
 from flask_wtf import CSRFProtect
 
 import form
@@ -8,6 +9,8 @@ from models import db, Alumnos
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 csrf = CSRFProtect()
+db.init_app(app)
+migrate = Migrate(app, db)
 
 
 @app.route("/")
@@ -84,6 +87,38 @@ def modificar():
     return None
 
 
+@app.route("/eliminar", methods=["GET", "POST"])
+def eliminar():
+    create_form = form.UserForm(request.form)
+    if request.method == "GET":
+        id = request.args.get("id")
+        alumno = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+
+        if alumno:
+            create_form.matricula.data = alumno.id
+            create_form.nombre.data = alumno.nombre
+            create_form.apaterno.data = alumno.apellido
+            create_form.correo.data = alumno.email
+            return render_template(
+                "eliminar.html",
+                form=create_form,
+                nombre=alumno.nombre,
+                apellido=alumno.apellido,
+                email=alumno.email,
+                matricula=alumno.id,
+            )
+
+    if request.method == "POST":
+        id = request.args.get("id")
+        alumno = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        if alumno:
+            db.session.delete(alumno)
+            db.session.commit()
+        return redirect(url_for("index"))
+
+    return render_template("eliminar.html", form=create_form)
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
@@ -91,7 +126,6 @@ def page_not_found(e):
 
 if __name__ == "__main__":
     csrf.init_app(app)
-    db.init_app(app)
 
     with app.app_context():
         db.create_all()
